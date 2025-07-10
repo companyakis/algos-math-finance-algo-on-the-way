fc6339451cb85b1183b76e20ff60f05aba2fc826
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-10 11:27:27 +0300

    Create part-z-0001-numpy-arrays2.py

---
+++ part-z-0001-numpy-arrays2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+ages = [22, 9, 11, 44, 87, 95, 3, 21]
+
+np_ages = np.array(ages)
+
+print(np_ages + 2)
+
+print(np_ages - 3)
+
+print(np_ages * 1.2)
+
+print(np_ages / 2)
+
+# [24 11 13 46 89 97  5 23]
+# [19  6  8 41 84 92  0 18]
+# [ 26.4  10.8  13.2  52.8 104.4 114.    3.6  25.2]
+# [11.   4.5  5.5 22.  43.5 47.5  1.5 10.5]
