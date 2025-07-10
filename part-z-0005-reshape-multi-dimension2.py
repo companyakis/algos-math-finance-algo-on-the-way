72b828e0cf84e9c23e922d293f961a880b24d8d8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-10 13:11:59 +0300

    Create part-z-0005-reshape-multi-dimension2.py

---
+++ part-z-0005-reshape-multi-dimension2.py
@@ -0,0 +1,11 @@
+quarterly_sales_by_months = np.array([[50000, 62000, 23000], [45000, 24000, 83000], [65000, 45000, 72000], [95000, 54700, 95600]])
+
+print(quarterly_sales_by_months.shape)
+
+print(f"February sales: {quarterly_sales_by_months[0][1]}")
+
+print(f"November sales: {quarterly_sales_by_months[3][1]}")
+
+# (4, 3)
+# February sales: 62000
+# November sales: 54700
