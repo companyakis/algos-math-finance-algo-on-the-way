fe6b93df32c565b796348b994d163acd0f726e84
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-17 22:41:38 +0300

    Create part-y-0003-complex-numbers2.py

---
+++ part-y-0003-complex-numbers2.py
@@ -0,0 +1,21 @@
+x = 4 + 5j
+
+y = complex(4, -5)
+
+print(x == y)
+
+print(x.real)
+
+print(y.real)
+
+# let's print imaginary parts
+
+print(x.imag)
+
+print(y.imag)
+
+# False
+# 4.0
+# 4.0
+# 5.0
+# -5.0
