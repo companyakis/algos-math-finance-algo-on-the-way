6a54960952e31305c1baa393a3ddbc986c3100d0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-22 14:42:19 +0300

    Create part-y-0006-list-manipulation2.py

---
+++ part-y-0006-list-manipulation2.py
@@ -0,0 +1,8 @@
+ages = [55, 47, 95, 78, 17, 65, 16, 54, 21, 12, 44, 14, 100, 8]
+
+can_vote = ["Yes" if age >= 18 else "No" for age in ages] 
+
+print(can_vote)
+
+# ['Yes', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No']
+
