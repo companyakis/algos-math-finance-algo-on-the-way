9fac6cfa5ea581cfd1009ccc197448c9d9f18e65
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-23 21:13:08 +0300

    Create part-y-0013-calculator-version1-1.py

---
+++ part-y-0013-calculator-version1-1.py
@@ -0,0 +1,29 @@
+def calculator(x, y, sign):
+
+  if (sign == "+"):
+
+    print(f"{x} + {y} = {x + y}") 
+
+  elif (sign == "-"):
+
+    print(f"{x} - {y} = {x - y}") 
+
+  elif (sign == "*"):
+
+    print(f"{x} * {y} = {x * y}")
+
+  elif (sign == "/" and y != 0):
+
+    print(f"{x} / {y} = {x / y}")
+
+  elif (sign == "/" and y == 0):
+
+    print("Divisor can't be zero!")
+
+  elif (sign == "%" and y != 0):
+
+    print(f"Remainder => {x} % {y} = {x % y}")
+
+  else:
+    
+    print("Unknown selection or wrong data types! Try again later...")
