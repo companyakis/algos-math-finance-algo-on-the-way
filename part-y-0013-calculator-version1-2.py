4dc0edf76ffd9253409a8f73028a1dff30bafc35
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-23 21:14:45 +0300

    Create part-y-0013-calculator-version1-2.py

---
+++ part-y-0013-calculator-version1-2.py
@@ -0,0 +1,15 @@
+calculator(12, 5, "%")
+
+calculator(1.2, 5, "+")
+
+calculator(1.2, 0, "/")
+
+calculator(1.2, 0, "&")
+
+"""
+Remainder => 12 % 5 = 2
+1.2 + 5 = 6.2
+Divisor can't be zero!
+Unknown selection or wrong data types! Try again later...
+
+"""
