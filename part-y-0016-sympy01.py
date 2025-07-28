496118acda84fe2fcbc9e2e92cec6924725218d5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 22:17:42 +0300

    Create part-y-0016-sympy01.py

---
+++ part-y-0016-sympy01.py
@@ -0,0 +1,13 @@
+import sympy as sym
+
+# create symbolic variables
+
+a, b, c = sym.symbols("a, b, c")
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# <class 'sympy.core.symbol.Symbol'>
+# <class 'sympy.core.symbol.Symbol'>
+# <class 'sympy.core.symbol.Symbol'>
