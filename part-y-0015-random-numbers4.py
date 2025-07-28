a826c73a3f9343057ae184432a597c73e8c2550d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 08:57:11 +0300

    Create part-y-0015-random-numbers4.py

---
+++ part-y-0015-random-numbers4.py
@@ -0,0 +1,20 @@
+num1 = np.random.ranf()
+
+print(num1)
+
+num2 = np.random.uniform(4, 5)
+
+print(num2)
+
+nums = np.random.uniform(-3, 0, 5)
+
+print(nums)
+
+"""
+0.8376577917990967
+
+4.773047378510387
+
+[-1.70671884 -0.47438757 -1.51835801 -0.88640534 -2.85122272]
+
+"""
